874c2c4f581e647d07627897bd2ec7b177fc6619
qlebris <quentin.lebris@gmail.com>
2020-11-28 15:47:38 +0100

    add bootstrap and jquery

--- assets/app.js
+++ assets/app.js
@@ -4,9 +4,10 @@
  * We recommend including the built version of this JavaScript file
  * (and its CSS file) in your base layout (base.html.twig).
  */
-
+const $ = require('jquery');
+require('bootstrap');
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import './styles/app.scss';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
