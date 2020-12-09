7454d0bbfe9465128f5325dd73a01b0ef3e8927b
qlebris <quentin.lebris@gmail.com>
2020-12-09 11:01:47 +0100

    allow to handle images as assets and add logo on header

--- assets/app.js
+++ assets/app.js
@@ -6,6 +6,7 @@
  */
 const $ = require('jquery');
 require('bootstrap');
+require('./navbar.js');
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
 
--- src/Controller/WebSiteController.php
+++ src/Controller/WebSiteController.php
@@ -15,11 +15,24 @@ class WebSiteController extends AbstractController
      * @Route("/", name="home")
      * @return Response
      */
-
     public function homePage(): Response
     {
         return $this->render('pages/home.html.twig');
     }
+
+    /**
+     * @param CategoryRepository $categoryRepository
+     * @return Response
+     */
+    public function navbar(CategoryRepository $categoryRepository): Response
+    {
+        $categories = $categoryRepository->findAll();
+
+        return $this->render('header.html.twig', [
+            'categories' => $categories
+        ]);
+    }
+
 
     /**
      * @param CategoryRepository $categoryRepository
